b2b47ad605fb076c4d2941f7fe10f8297ae30dfa
VanderArcadius <RJYoung7@gmail.com>
2022-08-25 15:03:53 -0700

    Refactored into partials to Dry code. Added JSON.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const data = require('./public/assets/pokemonlist.json');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+app.set('partials', path.join(__dirname, '/partials'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    const set = data.series[0].expansions[0];
+    const cards = set.cards;
+    // const { name } = set;
+    res.render('home', {set, cards });
+    // console.log(name);
+})
+
+app.get('/pokemongo', (req, res) => {
+    const set = data.series[0].expansions[0];
+    const cards = set.cards;
+    // const { name } = set;
+    res.render('home', {set, cards });
+    // console.log(name);
 })
 
 app.listen(3000, () => {
