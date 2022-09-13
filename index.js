d1910d31cff6504767a820628619ee1660559e83
VanderArcadius <RJYoung7@gmail.com>
2022-09-13 11:01:38 -0700

    Added functionality to connect to db

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const engine = require('ejs-mate');
+
+const Card = require('./models/card');
 const data = require('./public/assets/pokemonlist.json');
 
-app.use(express.static(path.join(__dirname, 'public')))
 
+main()
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN");
+    }).catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/pokemonCollectr');
+}
+
+app.engine('ejs', engine);
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+// app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('partials', path.join(__dirname, '/partials'));
+app.set('cards', path.join(__dirname, '/cards'));
 
 app.get('/', (req, res) => {
     const set = data.series[0].expansions[0];
     const cards = set.cards;
     // const { name } = set;
-    res.render('home', {set, cards });
+    res.render('cards/index', {set, cards });
     // console.log(name);
 })
 
