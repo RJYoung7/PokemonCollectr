c4a0122fdfb123b996c8e09848412899aca4abf4
VanderArcadius <RJYoung7@gmail.com>
2022-09-14 15:51:10 -0700

    Required Axios and tested api call

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const axios = require('axios');
 const engine = require('ejs-mate');
 
 const Card = require('./models/card');
@@ -31,11 +32,17 @@ app.use(methodOverride('_method'));
 app.set('partials', path.join(__dirname, '/partials'));
 app.set('cards', path.join(__dirname, '/cards'));
 
-app.get('/', (req, res) => {
-    const set = data.series[0].expansions[0];
-    const cards = set.cards;
+app.get('/', async (req, res) => {
+    // const set = data.series[0].expansions[0];
+    // const cards = set.cards;
+    const config = { Headers: { Authorization: 'a046ddc1-16d3-4bb1-a8b4-7a1be75dd132'}};
+    const {data} = await axios.get('https://api.pokemontcg.io/v2/sets', config);
+    const setArray = data.data;
+    console.log(setArray);
     // const { name } = set;
-    res.render('cards/index', {set, cards });
+    res.render('cards/index', { setArray });
+
+    // res.render('cards/index', {set, cards});
     // console.log(name);
 })
 
