6b4eaa72bbd9c30667180fd602ba5711e27dad88
VanderArcadius <RJYoung7@gmail.com>
2022-09-16 16:07:29 -0700

    Uncommented path line. Now using custom stylesheet

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('partials', path.join(__dirname, '/partials'));
 app.set('cards', path.join(__dirname, '/cards'));
