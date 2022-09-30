fea7021aa03b4bcfdf6e9f267286ad474649b15b
VanderArcadius <RJYoung7@gmail.com>
2022-09-30 12:38:38 -0700

    Added logic to send a post request

--- public/js/countFunctions.js
+++ public/js/countFunctions.js
@@ -1,3 +1,4 @@
+
 let downBtns = document.querySelectorAll('.downBtns');
 // console.log(btns);
 downBtns.forEach(btn => {
@@ -21,6 +22,37 @@ upBtns.forEach(btn => {
         let count = parseInt(spanCountStr);
         count++;
         spanBtn.innerText = `${count}`;
+        let pokemonId = e.target.parentNode.parentNode.parentNode.id;
+        console.log(e.target.parentNode.parentNode.parentNode)
+        console.log(pokemonId)
+        let countUpdate = {
+            pokemonId: e.target.parentNode.parentNode.parentNode.id,
+            countType: e.target.parentNode.id,
+            count: count
+        }
+
+        // axios.post('/set/swsh11', countUpdate).then(function(response) {
+        //     console.log("Response:",  response)
+        // })
+        // .catch(function(error) {
+        //     console.log(error)
+        // })
+        const params = new URLSearchParams();
+        params.append('pokemonId', e.target.parentNode.parentNode.parentNode.id);
+        params.append('countType', e.target.parentNode.id);
+        params.append('count', count);
+        axios.post('/set/swsh11', params);
+
+        // axios({
+        //     method: 'post',
+        //     url: '/set/swsh11',
+        //     data: {
+        //         id: 'swsh11',
+        //         count: '1'
+        //     }
+        // });
+
+        console.log(countUpdate);
         console.log('up clicked');
     })
 })
