dcfd6d0f8b41afde27737a6abe110f3081d37128
VanderArcadius <RJYoung7@gmail.com>
2022-10-03 18:50:41 -0700

    Updated post request logic and refactored

--- public/js/countFunctions.js
+++ public/js/countFunctions.js
@@ -1,63 +1,65 @@
 
 let downBtns = document.querySelectorAll('.downBtns');
-// console.log(btns);
+
+// Event listener to decrement the count of the specified card type
 downBtns.forEach(btn => {
     btn.addEventListener('click', (e) => {
+        // Target the span where the count resides
         const spanBtn = e.target.nextElementSibling.querySelector('span');
-        let spanCountStr = spanBtn.innerText;
-        let count = parseInt(spanCountStr);
-        if(count > 0)
+        // Parse the count
+        let count = parseInt(spanBtn.innerText);
+        // Decrement the count if it is greater than 0
+        if(count > 0) {
             count--;
-        spanBtn.innerText = `${count}`;
+            // Set the span to the updated count value
+            spanBtn.innerText = `${count}`;
+            // Get the data to send to the server
+            let countUpdate = {
+                pokemonId: e.target.parentNode.parentNode.parentNode.id,
+                countType: e.target.parentNode.id,
+                count: count
+            }
+    
+            // Make a post request to update the database with the new count data
+            axios.post('swsh11', countUpdate).then(function(response) {
+                console.log("Response:",  response)
+            })
+            .catch(function(error) {
+                console.log(error)
+            })
+        }
+        
         console.log('down clicked');
     })
 })
 
 let upBtns = document.querySelectorAll('.upBtns');
-// console.log(btns);
+
+// Event listener to increment the count of the specified card type
 upBtns.forEach(btn => {
     btn.addEventListener('click', (e) => {
+        // Target the span where the count resides
         const spanBtn = e.target.previousElementSibling.querySelector('span');
-        let spanCountStr = spanBtn.innerText;
-        let count = parseInt(spanCountStr);
+        // Parse the count
+        let count = parseInt(spanBtn.innerText);
+        // Increment the count
         count++;
+        // Set the span to the updated count value
         spanBtn.innerText = `${count}`;
-        let pokemonId = e.target.parentNode.parentNode.parentNode.id;
-        console.log(e.target.parentNode.parentNode.parentNode)
-        console.log(pokemonId)
+
+        // Get the data to send to the server
         let countUpdate = {
             pokemonId: e.target.parentNode.parentNode.parentNode.id,
             countType: e.target.parentNode.id,
             count: count
         }
 
-        // axios.post('/set/swsh11', countUpdate).then(function(response) {
-        //     console.log("Response:",  response)
-        // })
-        // .catch(function(error) {
-        //     console.log(error)
-        // })
-        const params = new URLSearchParams();
-        params.append('pokemonId', e.target.parentNode.parentNode.parentNode.id);
-        params.append('countType', e.target.parentNode.id);
-        params.append('count', count);
-        axios.post('/set/swsh11', params);
-
-        // axios({
-        //     method: 'post',
-        //     url: '/set/swsh11',
-        //     data: {
-        //         id: 'swsh11',
-        //         count: '1'
-        //     }
-        // });
-
-        console.log(countUpdate);
-        console.log('up clicked');
+        // Make a post request to update the database with the new count data
+        axios.post('swsh11', countUpdate).then(function(response) {
+            console.log("Response:",  response)
+        })
+        .catch(function(error) {
+            console.log(error)
+        })
     })
 })
-
-
-// btn.addEventListener('click', () => {
-//     console.log('clicked');
-// })
