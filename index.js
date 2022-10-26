f93a31aba95f2220174b99087919e05e9c13c375
VanderArcadius <RJYoung7@gmail.com>
2022-10-26 13:57:51 -0700

    Reformatted

--- index.js
+++ index.js
@@ -1,109 +1,109 @@
-const express = require('express');
-const engine = require('ejs-mate');
-const multer = require('multer');
-const axios = require('axios');
-const methodOverride = require('method-override');
-const pokemon = require('pokemontcgsdk');
-const mongoose = require('mongoose');
-const globals = require('./globals');
+const express = require("express");
+const engine = require("ejs-mate");
+const multer = require("multer");
+const axios = require("axios");
+const methodOverride = require("method-override");
+const pokemon = require("pokemontcgsdk");
+const mongoose = require("mongoose");
+const globals = require("./globals");
+const fs = require("fs");
 
-const Card = require('./models/card');
-const Set = require('./models/set');
+const Card = require("./models/card");
+const Set = require("./models/set");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname + '/uploads/'))
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now();
-        cb(null, file.originalname + '--' + uniqueSuffix);
-    }
-})
-  
-const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname + "/uploads/"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, file.originalname + "--" + uniqueSuffix);
+  },
+});
 
-pokemon.configure({apiKey: globals.API_KEY});
+const upload = multer({ storage: storage });
+
+pokemon.configure({ apiKey: globals.API_KEY });
 
 const app = express();
-const path = require('path');
-const { downloadCardImagesOfSet, uploadSetCards } = require('./JS/seed');
+const path = require("path");
+const { downloadCardImagesOfSet, uploadSetCards } = require("./JS/seed");
 
 main()
-    .then(() => {
-        console.log("MONGO CONNECTION OPEN");
-    }).catch(err => console.log(err));
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN");
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/pokemonCollectr');
+  await mongoose.connect("mongodb://localhost:27017/pokemonCollectr");
 }
 
+app.engine("ejs", engine);
 
-
-app.engine('ejs', engine);
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(methodOverride('_method'));
-app.use(express.static('multer'));
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'images')))
+app.use(methodOverride("_method"));
+app.use(express.static("multer"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "images")));
 
-app.set('partials', path.join(__dirname, '/partials'));
-app.set('cards', path.join(__dirname, '/cards'));
+app.set("partials", path.join(__dirname, "/partials"));
+app.set("cards", path.join(__dirname, "/cards"));
 
 // uploadSetCards('swsh11')
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
+  const sets = await Set.find({}).sort({ releaseDate: "desc" });
+  res.render("cards/index", { sets });
+});
 
-    const sets = await Set.find({}).sort({releaseDate: 'desc'});
-    res.render('cards/index', { sets });
-})
+app.get("/set/:id", async (req, res) => {
+  const { view } = req.query;
+  // Get the card data
+  const { id } = req.params;
+  // const view = req.params
+  const theSet = await Set.findOne({ id: id });
+  const cards = await Card.find({ set: theSet });
+  // fs.writeFileSync('cards.json', JSON.stringify(cards));
+  res.render("cards/set", { cards, theSet, view });
+});
 
-app.get('/set/:id', async (req, res) => {
-    const {view} = req.query;
-    // Get the card data
-    const {id} = req.params;
-    // const view = req.params
-    const theSet = await Set.findOne({id: id})
-    const cards = await Card.find({set: theSet});
+app.post("/set/:id", async (req, res) => {
+  // Destructure data from req body
+  const { pokemonId, countType, count } = req.body;
 
-    res.render('cards/set', { cards, theSet, view });
-})
+  // Search the database with the id of the card
+  const card = await Card.findOne({ id: `${pokemonId}` });
 
-app.post('/set/:id', async (req, res) => {
-    // Destructure data from req body
-    const {pokemonId, countType, count} = req.body;
+  // Update count based on count type
+  if (countType === "standardCount") {
+    card.counts.standardCount = count;
+  } else if (countType === "reverseCount") {
+    card.counts.reverseCount = count;
+  } else {
+    card.counts.holoCount = count;
+  }
 
-    // Search the database with the id of the card
-    const card = await Card.findOne({id: `${pokemonId}`});
+  // Sum counts of all card versions
+  let totalCounts =
+    card.counts.standardCount + card.counts.reverseCount + card.counts.hasHolo;
 
-    // Update count based on count type
-    if(countType === 'standardCount') {
-        card.counts.standardCount = count;
-    } else if (countType === 'reverseCount') {
-        card.counts.reverseCount = count;
-    } else {
-        card.counts.holoCount = count;
-    }
+  // Set owned to true if the count is above 0
+  card.owned = totalCounts > 0 ? true : false;
 
-    // Sum counts of all card versions
-    let totalCounts = card.counts.standardCount + card.counts.reverseCount + card.counts.hasHolo;
+  // Save the card data to the database
+  await card.save();
 
-    // Set owned to true if the count is above 0
-    card.owned = totalCounts > 0 ? true : false;
+  console.log("request received");
 
-    // Save the card data to the database
-    await card.save();
-
-    console.log('request received');
-    
-    res.status(200).end();
-    /** Need to send a response without updating page  **/
-})
+  res.status(200).end();
+  /** Need to send a response without updating page  **/
+});
 
 app.listen(3000, () => {
-    console.log('LISTENING ON PORT 3000');
-})
+  console.log("LISTENING ON PORT 3000");
+});
--- public/js/countFunctions.js
+++ public/js/countFunctions.js
@@ -7,74 +7,79 @@
 
 // const data = await getCardData('http://localhost:3000/json/test.json');
 let jsondata;
-fetch('http://localhost:3000/json/cards.json')
-    .then(function (u) {return u.json();}
-    ).then(function (json) {
-        jsondata = json;
-    });
+fetch("http://localhost:3000/json/cards.json")
+  .then(function (u) {
+    return u.json();
+  })
+  .then(function (json) {
+    jsondata = json;
+  });
 
-
-let downBtns = document.querySelectorAll('.downBtns');
+let downBtns = document.querySelectorAll(".downBtns");
 
 // Event listener to decrement the count of the specified card type
-downBtns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        // Target the span where the count resides
-        const spanBtn = e.target.nextElementSibling.querySelector('span');
-        // Parse the count
-        let count = parseInt(spanBtn.innerText);
-        // Decrement the count if it is greater than 0
-        if(count > 0) {
-            count--;
-            // Set the span to the updated count value
-            spanBtn.innerText = `${count}`;
-            // Get the data to send to the server
-            let countUpdate = {
-                pokemonId: e.target.parentNode.parentNode.parentNode.id,
-                countType: e.target.parentNode.id,
-                count: count
-            }
-            
-            // Make a post request to update the database with the new count data
-            axios.post('swsh11', countUpdate).then(function(response) {
-                console.log("Response:",  response)
-            })
-            .catch(function(error) {
-                console.log(error)
-            })
-        }
-        
-        console.log('down clicked');
-    })
-})
+downBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // Target the span where the count resides
+    const spanBtn = e.target.nextElementSibling.querySelector("span");
+    // Parse the count
+    let count = parseInt(spanBtn.innerText);
+    // Decrement the count if it is greater than 0
+    if (count > 0) {
+      count--;
+      // Set the span to the updated count value
+      spanBtn.innerText = `${count}`;
+      // Get the data to send to the server
+      let countUpdate = {
+        pokemonId: e.target.parentNode.parentNode.parentNode.id,
+        countType: e.target.parentNode.id,
+        count: count,
+      };
 
-let upBtns = document.querySelectorAll('.upBtns');
+      // Make a post request to update the database with the new count data
+      axios
+        .post("swsh11", countUpdate)
+        .then(function (response) {
+          console.log("Response:", response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+    console.log("down clicked");
+  });
+});
+
+let upBtns = document.querySelectorAll(".upBtns");
 
 // Event listener to increment the count of the specified card type
-upBtns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        // Target the span where the count resides
-        const spanBtn = e.target.previousElementSibling.querySelector('span');
-        // Parse the count
-        let count = parseInt(spanBtn.innerText);
-        // Increment the count
-        count++;
-        // Set the span to the updated count value
-        spanBtn.innerText = `${count}`;
+upBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // Target the span where the count resides
+    const spanBtn = e.target.previousElementSibling.querySelector("span");
+    // Parse the count
+    let count = parseInt(spanBtn.innerText);
+    // Increment the count
+    count++;
+    // Set the span to the updated count value
+    spanBtn.innerText = `${count}`;
 
-        // Get the data to send to the server
-        let countUpdate = {
-            pokemonId: e.target.parentNode.parentNode.parentNode.id,
-            countType: e.target.parentNode.id,
-            count: count
-        }
+    // Get the data to send to the server
+    let countUpdate = {
+      pokemonId: e.target.parentNode.parentNode.parentNode.id,
+      countType: e.target.parentNode.id,
+      count: count,
+    };
 
-        // Make a post request to update the database with the new count data
-        axios.post('swsh11', countUpdate).then(function(response) {
-            console.log("Response:",  response)
-        })
-        .catch(function(error) {
-            console.log(error)
-        })
-    })
-})
+    // Make a post request to update the database with the new count data
+    axios
+      .post("swsh11", countUpdate)
+      .then(function (response) {
+        console.log("Response:", response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+});
